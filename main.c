6b4d331975f15123f29475b445723ab8b7e4a25f
Null-Zer0-stack <52693491+Null-Zer0-stack@users.noreply.github.com>
2025-07-11 19:28:08 +0330

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+*	use this line if you are plan to using visual studio
+*	since msvc gives you compiler erorr due to not being 
+*	safe use scanf so therefore we force compiler ignore
+*	otherwise replace scanf_s with scanf in line 32 !
+*/
+#pragma warning(suppress: 4996)
+
+
+
+int main()
+{
+	unsigned int random_number = rand() ;
+	unsigned int * number = 0;
+	unsigned int loop;
+
+	for (loop = 0; loop < random_number; loop++)
+	{
+		unsigned int new_random_number = rand();
+		
+		number = number + new_random_number;
+
+			
+	}
+
+
+	printf("Your generated number is : %d", &number);
+	char hold[1];
+	scanf("%s", hold);
+
+	
+}
